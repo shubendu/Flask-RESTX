e7293d0518ceb2a974ebf508b26db35f968c6ef5
shubendu <biswasshubendu4@gmail.com>
2023-11-17 19:26:02 +0530

    updated CRUD operations

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from .extensions import db 
 
 class Course(db.Model):
-    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     
--- app/resources.py
+++ app/resources.py
@@ -1,4 +1,7 @@
 from flask_restx import Resource, Namespace
+from .models import Course, Student
+from .extensions import db
+from .api_models import course_model, student_model, course_input_model, student_input_model
 
 ns = Namespace("api")
 
@@ -6,3 +9,80 @@ ns = Namespace("api")
 class Hello(Resource):
     def get(self):
         return {"hello":"restx"}
+    
+@ns.route("/courses")
+class CourseAPIList(Resource):
+    @ns.marshal_list_with(course_model)
+    def get(self):
+        return Course.query.all()
+    
+    @ns.expect(course_input_model)
+    @ns.marshal_with(course_model)
+    def post(self):
+        print(ns.payload)
+        course = Course(name=ns.payload["name"])
+        db.session.add(course)
+        db.session.commit()
+        return course, 201
+    
+@ns.route("/courses/<int:id>")
+class CourseAPI(Resource):
+    @ns.marshal_with(course_model)
+    def get(self, id):
+        course = Course.query.get(id)
+        return course
+    
+    @ns.expect(course_input_model)
+    @ns.marshal_with(course_model)
+    def put(self,id):
+        course = Course.query.get(id)
+        course.name = ns.payload["name"]
+        db.session.commit()
+        return course
+    
+    def delete(self, id):
+        course = Course.query.get(id)
+        db.session.delete(course)
+        db.session.commit()
+        return {}, 204
+    
+        
+    
+@ns.route("/students")
+class StudentAPIList(Resource):
+    @ns.marshal_list_with(student_model)
+    def get(self):
+        return Student.query.all()
+    
+    @ns.expect(student_input_model)
+    @ns.marshal_with(student_model)
+    def post(self):
+        student = Student(name=ns.payload["name"], course_id = ns.payload["course_id"])
+        db.session.add(student)
+        db.session.commit()
+        return student, 201
+    
+@ns.route("/students/<int:id>")
+class StudentAPI(Resource):
+    @ns.marshal_with(student_model)
+    def get(self,id):
+        student = Student.query.get(id)
+        return student
+    
+    @ns.expect(student_input_model)
+    @ns.marshal_with(student_model)
+    def put(self,id):
+        student = Student.query.get(id)
+        student.name = ns.payload["name"]
+        student.course_id = ns.payload["course_id"]
+        db.session.commit()
+        return student
+    
+    def delete(self, id):
+        student = Student.query.get(id)
+        db.session.delete(student)
+        db.session.commit()
+        return {}, 204
+    
+    
+        
